9325d176c2737adc19657e7f2f954dd2af9d14ea
aneesh17 <aneesh.ravishankar@gmail.com>
2015-11-27 14:31:02 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,7 +11,29 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
+void reverse(char*, int, int);
+void str_words_in_rev(char *input, int len){	
+	int index, prevIndex = 0;
+	reverse(input, 0, len - 1);		
+	for (index = 0; index <= len; index++)
+	{
+		if (input[index] == 32 || index == len)
+		{			
+			reverse(input, prevIndex, index - 1);
+			prevIndex = index + 1;
+		}
+	}
+}
 
-void str_words_in_rev(char *input, int len){
-	
+void reverse(char* inp, int start, int end)
+{	
+	int length = end - start + 1;
+	char temp;
+	int counter;
+	for (counter = 1; counter <= length / 2; counter++)
+	{
+		temp = inp[start];
+		inp[start++] = inp[end];
+		inp[end--] = temp;		
+	}
 }
